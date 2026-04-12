d7c0789c73ea6686b39877563a1477f84b0358fa
Yogye Berry <yogyeberry@gmail.com>
2026-04-13 00:14:08 +0530

    fixes

--- js/theme.js
+++ js/theme.js
@@ -3,19 +3,19 @@ let btn = document.getElementById("themeToggle");
 let saved = localStorage.getItem("theme");
 if (saved === "light") {
     document.body.classList.add("light-mode");
-    btn.innerText = "🌙 Dark";
+    btn.innerText = "Dark";
 } else {
-    btn.innerText = "☀️ Light";
+    btn.innerText = "Light";
 }
 
 btn.addEventListener("click", function() {
     if (document.body.classList.contains("light-mode")) {
         document.body.classList.remove("light-mode");
         localStorage.setItem("theme", "dark");
-        btn.innerText = "☀️ Light";
+        btn.innerText = "Light";
     } else {
         document.body.classList.add("light-mode");
         localStorage.setItem("theme", "light");
-        btn.innerText = "🌙 Dark";
+        btn.innerText = "Dark";
     }
 });
